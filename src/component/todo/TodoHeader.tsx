00deb08134316ac6a6e478347ea4a90aa6450cbe
romanocast <castagnoli@simpit.ch>
2021-08-27 16:43:05 +0200

    fixing

--- src/component/todo/TodoHeader.tsx
+++ src/component/todo/TodoHeader.tsx
@@ -1,15 +1,41 @@
 import React from "react";
 
+import {AppBar, Toolbar, Typography, Button} from "@material-ui/core"
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+
 const TodoHeader: React.FC = () => {
+    const classes = useStyles();
     return (
-        <>
-        <h1>ToDo Application</h1>
-        <ul>
-            <li><a href="/about">About Page</a></li>
-            <li><a href="/todo">Todo Page</a></li>
-        </ul>
-        </>
+      
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            ToDo Application
+          </Typography>
+          <Button color="inherit">ToDo</Button>
+          <Button color="inherit">About</Button>
+        </Toolbar>
+      </AppBar>
+    
+
+
+
+      
     );
 }
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 export default TodoHeader
