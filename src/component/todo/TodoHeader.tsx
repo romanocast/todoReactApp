c0e3653b91fdc8492ec3767df033baa21ac58e5d
romanocast <87472231+romanocast@users.noreply.github.com>
2021-08-28 17:52:54 +0200

    Revert "didSomeOngoing"

--- src/component/todo/TodoHeader.tsx
+++ src/component/todo/TodoHeader.tsx
@@ -2,12 +2,10 @@ import React from "react";
 
 import {AppBar, Toolbar, Typography, Button} from "@material-ui/core"
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { useHistory } from "react-router-dom";
 
 
 const TodoHeader: React.FC = () => {
     const classes = useStyles();
-    const history = useHistory(); 
     return (
       
       <AppBar position="static">
@@ -15,12 +13,9 @@ const TodoHeader: React.FC = () => {
           <Typography variant="h6" className={classes.title}>
             ToDo Application
           </Typography>
-          <Button color="inherit"
-                  onClick={() => {history.push('/todo')}}
-                  >ToDo</Button>
-          <Button color="inherit"
-                  onClick={() => {history.push('/about')}}
-                  >About</Button>
+          <Button color="inherit">ToDo</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Testing</Button>
         </Toolbar>
       </AppBar>
     
